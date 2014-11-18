60d7022dbfda27234febdcdf9393710e9a32d87f
unknown <marcus.vinicius@N2012005721.PRF.SEDE>
2014-11-18 10:37:09 -0200

    File updated with the code for the programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The function stores a matrix which is invertible.
+## If the given matrix is not invertible, the function returns the sentence "Your matrix is not invertible. The determinant is equal 0."
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(m = matrix()) {
+  if (det(m)!=0){
+    i <- NULL
+    set <- function(matrix) {
+      m <<- matrix
+      i <<- NULL
+    }
+    get <- function() {
+      m
+    }
+    setinverse <- function(inverse) {
+      i <<- inverse
+    }
+    getinverse <- function(){
+      i
+    } 
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
+  } else { print("Your matrix is not invertible. The determinant is equal 0.")}
+  
 }
 
 
-## Write a short comment describing this function
+## This function returns the inverse matrix of the given matrix stored in cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data)
+  m
 }
