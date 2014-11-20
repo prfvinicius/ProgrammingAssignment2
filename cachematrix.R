3041212c9af303c2bd0f6acb15f5df21e8a36af3
unknown <marcus.vinicius@N2012005721.PRF.SEDE>
2014-11-20 16:30:49 -0200

    Code for the programming assignment 2 updated

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,8 @@
-## The function stores a matrix which is invertible.
-## If the given matrix is not invertible, the function returns the sentence "Your matrix is not invertible. The determinant is equal 0."
+## The function stores in cache a matrix which is invertible.
+## The first step is to identify that the matrix is invertible. If the given matrix is not invertible, the function returns the sentence "Your matrix is not invertible. The determinant is equal 0 or the matrix is not square"
 
 makeCacheMatrix <- function(m = matrix()) {
-  if (det(m)!=0){
+  if (det(m)!=0 & nrow(m)==ncol(m)){
     i <- NULL
     set <- function(matrix) {
       m <<- matrix
@@ -20,9 +20,10 @@ makeCacheMatrix <- function(m = matrix()) {
     list(set = set, get = get,
          setinverse = setinverse,
          getinverse = getinverse)
-  } else { print("Your matrix is not invertible. The determinant is equal 0.")}
+  } else { print("Your matrix is not invertible. The determinant is equal 0 or the matrix is not square.")}
   
 }
+
 
 
 ## This function returns the inverse matrix of the given matrix stored in cache.
